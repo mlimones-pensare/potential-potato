b87c66c5ff1c78ae4983ea5d3e653c782794fd92
Manuel Limones <mlimones@pensare.mx>
2018-10-31 20:46:51 -0600

    Add admin page

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,9 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container-fluid">
+    <div class="row row justify-content-md-center">
+        <div class="col-auto">
+            <ul class="list-group">
+                <li class="list-group-item"><a href="">Franquiciatarios</a></li>
+                <li class="list-group-item"><a href="">Tiendas</a></li>
+                <li class="list-group-item"><a href="">Agregar Comentarios</a></li>
+                <li class="list-group-item"><a href="">Alta Productos</a></li>
+                <li class="list-group-item"><a href="">Editar Productos</a></li>
+                <li class="list-group-item"><a href="">Notas de Venta</a></li>
+                <li class="list-group-item"><a href="">Administradores</a></li>
+                <li class="list-group-item"><a href="">Proveedores</a></li>
+                <li class="list-group-item"><a href="">Reportes</a></li>
+                <li class="list-group-item"><a href="">Productos Calificados</a></li>
+                <li class="list-group-item"><a href="">Comentarios En Pagina</a></li>
+                <li class="list-group-item"><a href="">Presolicitud de Franquicia</a></li>
+                <li class="list-group-item"><a href="">Envio Mails</a></li>
+                <li class="list-group-item"><a href="">Etiquetas</a></li>
+                <li class="list-group-item"><a href="">Guias</a></li>
+                <li class="list-group-item"><a href="">Generar codigos</a></li>
+                <li class="list-group-item"><a href="">Editar Portal</a></li>
+                <li class="list-group-item"><a href="">Precios ChipChop</a></li>
+                <li class="list-group-item"><a href="">MultiPrecios</a></li>
+                <li class="list-group-item"><a href="">Salir</a></li>
+            </ul>
+        </div>
+        <div class="col-8">
             <div class="card">
                 <div class="card-body">
                     Welcome!
